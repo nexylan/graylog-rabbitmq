8db766eadb42dc637a3ec3e5665b75cd21558bab
Henri Devigne <222364@supinfo.com>
2018-08-03 20:13:13 +0200

    Install rabbitmq and prepare the sender ( beta )

--- src/main/java/net/nexylan/graylog/rabbitmq/senders/RabbitMQSender.java
+++ src/main/java/net/nexylan/graylog/rabbitmq/senders/RabbitMQSender.java
@@ -2,11 +2,24 @@ package net.nexylan.graylog.rabbitmq.senders;
 
 import org.graylog2.plugin.Message;
 
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+
 public class RabbitMQSender implements Sender {
 
     private String host;
     private int port;
     private String queue;
+
+    //RabbitMQ objects
+    private ConnectionFactory factory;
+    private Connection connection;
+    private Channel channel;
 
     private boolean is_initialized;
 
@@ -22,18 +35,55 @@ public class RabbitMQSender implements Sender {
     @Override
     public void initialize()
     {
+        this.factory = new ConnectionFactory();
+        this.factory.setHost(this.host);
+        this.factory.setPort(this.port);
 
+        try {
+            this.connection = this.factory.newConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+        try {
+            this.channel = this.connection.createChannel();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            this.channel.queueDeclare(this.queue, false, false, false, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void stop()
     {
-
+        try {
+            this.channel.close();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            this.connection.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void send(Message message)
     {
+        try {
+            this.channel.basicPublish("", this.queue, null, Message.FIELD_FULL_MESSAGE.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
