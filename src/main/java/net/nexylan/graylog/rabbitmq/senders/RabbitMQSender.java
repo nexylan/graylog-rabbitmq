df97bc7ac802f5b79ca083ba4daee6d7b727b142
Henri Devigne <222364@supinfo.com>
2018-08-04 19:46:26 +0200

    Add info/error log about the plugin's process

--- src/main/java/net/nexylan/graylog/rabbitmq/senders/RabbitMQSender.java
+++ src/main/java/net/nexylan/graylog/rabbitmq/senders/RabbitMQSender.java
@@ -8,6 +8,8 @@ import com.rabbitmq.client.Channel;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class RabbitMQSender implements Sender {
@@ -17,11 +19,12 @@ public class RabbitMQSender implements Sender {
     private String queue;
 
     //RabbitMQ objects
-    private ConnectionFactory factory;
     private Connection connection;
     private Channel channel;
 
-    private boolean is_initialized;
+    private static final Logger LOG = LoggerFactory.getLogger(RabbitMQSender.class);
+
+    private boolean is_initialized = false;
 
     public RabbitMQSender(String host, int port, String queue)
     {
@@ -35,28 +38,37 @@ public class RabbitMQSender implements Sender {
     @Override
     public void initialize()
     {
-        this.factory = new ConnectionFactory();
-        this.factory.setHost(this.host);
-        this.factory.setPort(this.port);
+        ConnectionFactory factory;
+        factory = new ConnectionFactory();
+        factory.setHost(this.host);
+        factory.setPort(this.port);
 
         try {
-            this.connection = this.factory.newConnection();
+            this.connection = factory.newConnection();
+            LOG.info("[RabbitMQ] Successfully connected to the server.");
         } catch (IOException e) {
+            LOG.error("[RabbitMQ] Failed to connect to RabbitMQ Server.");
             e.printStackTrace();
         } catch (TimeoutException e) {
             e.printStackTrace();
+            LOG.error("[RabbitMQ] The RabbitMQ Server timed out.");
         }
         try {
             this.channel = this.connection.createChannel();
+            LOG.info("[RabbitMQ] The channel have been successfully created.");
         } catch (IOException e) {
+            LOG.error("[RabbitMQ] An error occurred during the channel creation.");
             e.printStackTrace();
         }
 
         try {
             this.channel.queueDeclare(this.queue, false, false, false, null);
+            LOG.info("[RabbitMQ] The queue have been successfully created.");
         } catch (IOException e) {
+            LOG.error("[RabbitMQ] Impossible to declare the queue.");
             e.printStackTrace();
         }
+        this.is_initialized = true;
     }
 
     @Override
@@ -65,13 +77,16 @@ public class RabbitMQSender implements Sender {
         try {
             this.channel.close();
         } catch (TimeoutException e) {
+            LOG.error("[RabbitMQ] An error occurred while closing the channel.");
             e.printStackTrace();
         } catch (IOException e) {
+            LOG.error("[RabbitMQ] An error occurred.");
             e.printStackTrace();
         }
         try {
             this.connection.close();
         } catch (IOException e) {
+            LOG.error("[RabbitMQ] An error occurred while closing the connection.");
             e.printStackTrace();
         }
     }
@@ -82,6 +97,7 @@ public class RabbitMQSender implements Sender {
         try {
             this.channel.basicPublish("", this.queue, null, message.getField(Message.FIELD_FULL_MESSAGE).toString().getBytes());
         } catch (IOException e) {
+            LOG.error("[RabbitMQ] An error occurred while publishing message.");
             e.printStackTrace();
         }
 
